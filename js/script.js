dad3149de6e646ba7ffee3e54c27655229e7a2cf
eugeniomalavasi <eugenio.malavasi@gmail.com>
2024-03-27 16:20:47 +0100

    feat: now fizz and buzz are working as needed but fizzbuzz repeat with fizz

--- js/script.js
+++ js/script.js
@@ -1,5 +1,15 @@
 // TEST
 // alert("ciao come va?")
 for (let i = 1; i <= 100; i++) {
-    console.log(i);
+    
+    if (i % 3 === 0) {
+        console.log(i, "Fizz");
+    } else if (i % 5 === 0) {
+        console.log(i, "Buzz")
+    } else {
+        console.log(i);
+    }
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log(i, "FizzBuzz");
+    } 
 }

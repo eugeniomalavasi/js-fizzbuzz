f08eb5590e10cab15c71ede27ee9e98a279bc2ee
eugeniomalavasi <eugenio.malavasi@gmail.com>
2024-03-27 18:10:01 +0100

    fix: added bgClass to have the string to add with classlist

--- js/script.js
+++ js/script.js
@@ -2,15 +2,28 @@
 // alert("ciao come va?")
 
 // LOGICA
+
 for (let i = 1; i <= 100; i++) {
+    let text;  
     if (i % 3 === 0 && i % 5 === 0) { // come prima condizione deve analizzare se i numeri sono divisibili per 3 e per 5 e se si stampa FizzBuzz
-        console.log(i, "FizzBuzz");
+        text = "FizzBuzz";
+        let bgClass = "fizzbuzz";
     } else if (i % 3 === 0) { // analizza se divisibile per 3 e stampa Fizz
-        console.log(i, "Fizz");
+        text = "Fizz";
+        bgClass = "fizz";
     } else if (i % 5 === 0) { // analizza se divisibile per 5 e stampa Buzz
-        console.log(i, "Buzz")
+        text = "Buzz";
+        bgClass = "buzz";
     } else {
-        console.log(i); // se non si verificano le condizioni precedenti stampa il numero
+        text = i;
+        bgClass = "number";
     }
+    // OUTPUT
+    const rowElement = document.querySelector(".container")
+    const boxElem = document.createElement("div");
+    boxElem.innerHTML = text;
+    boxElem.classList.add("square")
+    boxElem.classList.add(bgClass)
+    rowElement.append(boxElem)
+
 }
-// OUTPUT

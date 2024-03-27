3868563532f45f02c987b39404cd60d4fbcd8b5f
eugeniomalavasi <eugenio.malavasi@gmail.com>
2024-03-27 17:10:24 +0100

    feat: box are styled and ready to be inserted with js in the dom

--- js/script.js
+++ js/script.js
@@ -1,19 +1,16 @@
 // TEST
 // alert("ciao come va?")
+
+// LOGICA
 for (let i = 1; i <= 100; i++) {
-    
-    if (i % 3 === 0 && i % 5 === 0) {
+    if (i % 3 === 0 && i % 5 === 0) { // come prima condizione deve analizzare se i numeri sono divisibili per 3 e per 5 e se si stampa FizzBuzz
         console.log(i, "FizzBuzz");
-    } else if (i % 3 === 0) {
+    } else if (i % 3 === 0) { // analizza se divisibile per 3 e stampa Fizz
         console.log(i, "Fizz");
-    } else if (i % 5 === 0) {
+    } else if (i % 5 === 0) { // analizza se divisibile per 5 e stampa Buzz
         console.log(i, "Buzz")
     } else {
-        console.log(i);
+        console.log(i); // se non si verificano le condizioni precedenti stampa il numero
     }
 }
-
-
-// se i è divisibile per 3 allora fai Fizz
-// se i è divisibile per 5 allora fai Buzz
-// se i è divisibile per 3 e per 5 fai FizzBuzz ma non fare 
+// OUTPUT

5b14fdd44c3691d85bd2a93082aac27a13109f49
eugeniomalavasi <eugenio.malavasi@gmail.com>
2024-03-27 16:40:42 +0100

    fix: now FizzBuzz works as needed and is printed only when necessesary

--- js/script.js
+++ js/script.js
@@ -2,14 +2,18 @@
 // alert("ciao come va?")
 for (let i = 1; i <= 100; i++) {
     
-    if (i % 3 === 0) {
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log(i, "FizzBuzz");
+    } else if (i % 3 === 0) {
         console.log(i, "Fizz");
     } else if (i % 5 === 0) {
         console.log(i, "Buzz")
     } else {
         console.log(i);
     }
-    if (i % 3 === 0 && i % 5 === 0) {
-        console.log(i, "FizzBuzz");
-    } 
 }
+
+
+// se i è divisibile per 3 allora fai Fizz
+// se i è divisibile per 5 allora fai Buzz
+// se i è divisibile per 3 e per 5 fai FizzBuzz ma non fare 
